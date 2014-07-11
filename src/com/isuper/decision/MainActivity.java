80d73416fbc9fe02d89b175ea5cdff8f9c3ac14e
I <I@Jeremy>
2014-07-11 14:35:53 +0800

    bottom

--- src/com/isuper/decision/MainActivity.java
+++ src/com/isuper/decision/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_home);
 		Bmob.initialize(this, "ff8c3dbc926628cacf54f9c9bbf8dbab");
 	}
 }
